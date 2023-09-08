3ddfabdb52fb4e2c7dfb401c3d759f8a61146a1c
Aletheaaa1 <1120495490@qq.com>
2023-09-09 03:10:31 +0800

    体积光（挖坑）

---
+++ Examples/VolumetricLight.cpp
@@ -0,0 +1,262 @@
+//#define STB_IMAGE_IMPLEMENTATION
+//#include <stb_image.h>
+#include "../VertexArrayObject.h"
+#include "../VertexBufferObject.h"
+#include "../Shader.h"
+#include "../Camera.h"
+#include "../Model.h"
+#include "../Texture.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
+#include <vector>
+#include <format>
+#include <random>
+
+Camera camera{ glm::vec3(0, 10.0f, 10.0f), 0.0f, 0.0f, glm::vec3(0, 1.0f, 0) };
+constexpr unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+constexpr unsigned int SCREEN_WIDHT = 800, SCREEN_HEIGHT = 600;
+
+void RenderQuad();
+void RenderCube();
+
+#pragma region Data
+float quadVertices[] = {
+	// positions						  // texture Coords
+	-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+	-1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+	 1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+	 1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+	 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+	-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+};
+
+float vertices[] = {
+	// back face
+	-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+	 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+	 1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right
+	 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+	-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+	-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+	// front face
+	-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+	 1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+	 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+	 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+	-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+	-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+	// left face
+	-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+	-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+	-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+	-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+	-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+	-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+	// right face
+	 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+	 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+	 1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right
+	 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+	 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+	 1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+	 // bottom face
+	 -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+	  1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+	  1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+	  1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+	 -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+	 -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+	 // top face
+	 -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+	  1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+	  1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right
+	  1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+	 -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+	 -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left
+};
+#pragma endregion Data
+
+#pragma region Interaction
+float x_last, y_last;
+bool first_mouse = true;
+void MouseCallback(GLFWwindow* window, double x_pos, double y_pos)
+{
+	if (first_mouse == true)
+	{
+		x_last = x_pos;
+		y_last = y_pos;
+		first_mouse = false;
+	}
+
+	float x_offset = x_pos - x_last;
+	float y_offset = y_pos - y_last;
+
+	x_last = x_pos;
+	y_last = y_pos;
+
+	camera.UpdateCameraView(x_offset, y_offset);
+	camera.GetViewMatrix();
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+	float velocity = 0.005f;
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		camera.speedZ = -velocity;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		camera.speedZ = velocity;
+	}
+	else
+	{
+		camera.speedZ = 0.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		camera.speedX = -velocity;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		camera.speedX = velocity;
+	}
+	else
+	{
+		camera.speedX = 0.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+	{
+		camera.speedY = velocity;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+	{
+		camera.speedY = -velocity;
+	}
+	else
+	{
+		camera.speedY = 0.0f;
+	}
+}
+
+bool reload_shader;
+void ReLoad(GLFWwindow* window, int focus)
+{
+	if (focus == 1)
+	{
+		reload_shader = true;
+	}
+	else
+	{
+		reload_shader = false;
+	}
+}
+#pragma endregion Interaction
+
+void Render(Shader& shader);
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+
+	window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, MouseCallback);
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "glad error!" << std::endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_MULTISAMPLE);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		auto start = glfwGetTime();
+		ProcessInput(window);
+		glfwSetWindowFocusCallback(window, ReLoad);
+		if (reload_shader == true)
+		{
+		}
+
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 50.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
+
+		camera.UpdateCameraPosition();
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+		/* Poll for and process events */
+		glfwPollEvents();
+
+		auto end = glfwGetTime();
+		double frame = 1.0f / (end - start);
+
+		glfwSwapInterval(0);
+		//frame <= 60 ? glfwSwapInterval(1) : glfwSwapInterval(1);
+
+		glfwSetWindowTitle(window, (std::to_string(int(frame)) + "fps").data());
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void RenderCube()
+{
+	VertexBufferObject vbo{ vertices, sizeof(vertices) };
+	vbo.Bind();
+
+	VertexArrayObject vao{};
+	vao.AddBuffer(0, 3, GL_FLOAT, false, 8 * sizeof(float), 0);
+	vao.AddBuffer(1, 3, GL_FLOAT, false, 8 * sizeof(float), 3 * sizeof(float));
+	vao.AddBuffer(2, 2, GL_FLOAT, false, 8 * sizeof(float), 6 * sizeof(float));
+	vao.Bind();
+
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+void RenderQuad()
+{
+	VertexBufferObject vbo{ quadVertices, sizeof(quadVertices) };
+	vbo.Bind();
+
+	VertexArrayObject vao{};
+	vao.AddBuffer(0, 3, GL_FLOAT, false, 8 * sizeof(float), 0);
+	vao.AddBuffer(1, 3, GL_FLOAT, false, 8 * sizeof(float), 3 * sizeof(float));
+	vao.AddBuffer(2, 2, GL_FLOAT, false, 8 * sizeof(float), 6 * sizeof(float));
+	vao.Bind();
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+}
